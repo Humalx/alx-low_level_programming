fd3bc756b5e5022af75fa2ac510460e086dbd579
root <Humanbtk@gmail.com>
2022-06-16 13:30:38 -0700

    n

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,14 @@
 */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putcchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
 	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
